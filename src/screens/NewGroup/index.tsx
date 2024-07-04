10dcaf0d6a8fd75d62057e11d5a4af2b4295751a
WellyngtonMS <wellyngtoncatarino@gmail.com>
2024-07-04 03:20:48 -0300

    use groupCreate in NewGroup screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,13 +5,30 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { Alert } from 'react-native';
+import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
   const navigation = useNavigation();
 
-  function handleNewGroup() {
-    navigation.navigate('players', { group });
+  async function handleNewGroup() {
+    if (group.trim().length === 0) {
+      return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+    }
+    
+    try {
+      await groupCreate(group);
+      navigation.navigate('players', { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar o grupo.');
+        console.log(error);
+      }
+    }
   }
 
   return (
