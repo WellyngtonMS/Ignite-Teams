525c1cadddd3d75fec660cf824c125f26ba976b0
WellyngtonMS <wellyngtoncatarino@gmail.com>
2024-07-04 05:57:36 -0300

    fix: corrections to styling in groups screen

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,5 +4,5 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  padding: 24px;
+  padding: 0 24px;
 `;
